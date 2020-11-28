fb08f9d04732a93fdddb3d75ba6a52495ef796f7
Harsh Khandelwal <harsh2602@gmail.com>
2020-11-28 14:33:21 +0800

    chore: initial commit

---
+++ marketing/src/components/Landing.js
@@ -0,0 +1,161 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import MaterialLink from '@material-ui/core/Link';
+import { Link } from 'react-router-dom';
+
+function Copyright() {
+  return (
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright ©️ '}
+      <MaterialLink component={Link} to='/' color='inherit'>
+        Your Website
+      </MaterialLink>{' '}
+      {new Date().getFullYear}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    a: {
+      textDecoration: 'none',
+    },
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Landing() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <main>
+        <div className={classes.heroContent}>
+          <Container maxWidth='sm'>
+            <Typography
+              component='h1'
+              variant='h2'
+              align='center'
+              color='textPrimary'
+              gutterBottom>
+              Home Page
+            </Typography>
+
+            <Typography
+              variant='h5'
+              align='center'
+              color='textSecondary'
+              paragraph>
+              Some interesting things to follow. Make it short and sweet but not
+              too short although can be extra sweet 😃
+            </Typography>
+
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify='center'>
+                <Grid item>
+                  <Link to='/pricing'>
+                    <Button variant='contained' color='primary'>
+                      Pricing
+                    </Button>
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link to='/pricing'>
+                    <Button variant='contained' color='secondary'>
+                      Pricing
+                    </Button>
+                  </Link>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+
+        <Container className={classes.cardGrid} maxWidth='md'>
+          {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image='https://source.unsplash.com/random'
+                  title='Image title'
+                  alt='image from unspalsh'
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant='h5' component='h2'>
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is a media card. You could use this section to express
+                    yourself.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size='small' color='primary'>
+                    View
+                  </Button>
+                  <Button size='small' color='primary'>
+                    Edit
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Container>
+      </main>
+
+      <footer className={classes.footer}>
+        <Typography variant='h6' align='center' gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant='subtitle1'
+          align='center'
+          color='textSecondary'
+          component='p'>
+          Something that suits the purpose of footer!
+        </Typography>
+        <Copyright />
+      </footer>
+    </>
+  );
+}
